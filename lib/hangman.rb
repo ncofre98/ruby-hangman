e09976d91856471fc18c21988f2a306ba49558d9
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-04-16 17:54:24 -0600

    States of hangman added in UI

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,11 +1,13 @@
 class Hangman
+  MAX_ATTEMPTS = 6
 
-  def initialize
-    p random_word
+  def play
+    secret_word = random_word
+    current_attempt = 0
   end
 
 
   def random_word
-    File.readlines('./lib/words.txt').select {|word| word.size.between?(6, 13)}.sample.strip
+    File.readlines('./lib/words.txt').select { |word| word.size.between?(6, 13)}.sample.strip
   end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require_relative 'lib/ui'
 require_relative 'lib/hangman.rb'
 
 game = Hangman.new
+game.play
