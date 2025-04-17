6002f5bbc654a4d3a906668adcd20f2ce242a0aa
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-04-17 16:16:21 -0600

    Basic functionality working

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,11 +1,33 @@
 class Hangman
-  MAX_ATTEMPTS = 6
+  MAX_ATTEMPTS = 7
 
   def play
     secret_word = random_word
-    current_attempt = 0
+    guessed = "_" * secret_word.size
+    attempts = 0
+
+    puts secret_word
+    UI.welcome_message
+    while (attempts < MAX_ATTEMPTS && guessed != secret_word)
+      UI.drawing(attempts, guessed)
+      char_guess = UI.instruction
+      if (secret_word.include?(char_guess))
+        guessed = unblank(secret_word, guessed, char_guess)
+      else
+        attempts += 1
+      end
+    end
+
+    UI.drawing(attempts, guessed)
+    UI.results(guessed == secret_word ? true : false, secret_word)
   end
 
+  def unblank(secret_word, guessed, char_guess)
+    secret_word.split('').each_with_index do |c, idx|
+      guessed[idx] = char_guess if c == char_guess
+    end
+    guessed
+  end
 
   def random_word
     File.readlines('./lib/words.txt').select { |word| word.size.between?(6, 13)}.sample.strip
--- lib/ui.rb
+++ lib/ui.rb
@@ -1,5 +1,13 @@
 module UI
   HANGMAN_STATES = [
+    '''
+  +---+
+      |
+      |
+      |
+      |
+      |
+=========''',
   '''
   +---+
   |   |
@@ -54,6 +62,39 @@ module UI
 
 
   def self.current_state(number_of_attempt)
-    HANGMAN_STATES[number_of_attempt]
+    puts HANGMAN_STATES[number_of_attempt]
+  end
+
+  def self.instruction
+    puts "\nType in the letter of your guess followed by ENTER"
+    gets.chomp
+  end
+
+  def self.welcome_message
+    puts <<~INSTRUCTIONS
+    Welcome to Hangman
+    Guess the secret word one letter at a time.
+    You have 6 attempts — one wrong guess will make a new drawing
+    If you lose all 6, the game is over.
+    Good luck!
+    INSTRUCTIONS
+  end
+
+  def self.hidden_word(word)
+    puts word
+  end
+
+  def self.drawing(attempts, word)
+    current_state(attempts)
+    hidden_word(word)
+  end
+
+  def self.results(winner, secret_word)
+    puts
+    if (winner)
+      puts "Congratulations, you guessed the secret word"
+    else
+      puts "Too bad, the secret word were #{secret_word}"
+    end
   end
 end
